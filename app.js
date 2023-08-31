616b50bf891cf7f9a8ab56bf0fd56b48893d1ae2
SANDRO WUTHRICH <aleksi.w@protonmail.com>
2023-08-31 15:49:55 +0300

    charts testaus

--- app.js
+++ app.js
@@ -22,20 +22,31 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 
 // URL ROUTES
+
+// Route to home page
 app.get('/', (req, res) => {
+    // Handlebars needs a key to show data on page, json is a good way to send it
     let homePageData = {
         'price': 31.25,
         'wind': 2,
         'temperature': 18
     };
+    // Render index.handlebars and send dynamic data to the page
     res.render('index', homePageData)
 });
-
+// Route to hourly data page
 app.get('/hourly', (req, res) => {
-    let hourlyPageData = {
-        'hour': 13,
-        'price': 31.44
-    };
+    // Data will be presented in a table. To loop all rows we need a key for table and for column
+    let hourlyPageData = {'tableData' : [
+        {'hour': 13,
+        'price': 31.44},
+        {'hour': 14,
+        'price': 32.10},
+        {'hour': 15,
+        'price': 30.50},
+        {'hour': 16,
+        'price': 29.99}
+    ] };
     res.render('hourly', hourlyPageData)
 });
 
