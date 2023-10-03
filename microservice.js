9356d60299de3417946cb9ccb4b5b3285f5f8bda
SANDRO WUTHRICH <aleksi.w@protonmail.com>
2023-10-03 15:45:29 +0300

    logger included in microservice

--- microservice.js
+++ microservice.js
@@ -1,6 +1,7 @@
 const Pool = require('pg').Pool;
 const cron = require('node-cron');
 const priceService = require('./priceService');
+const logger = require('./logger')
 
 const pool = new Pool({
     user: 'postgres',
@@ -11,13 +12,23 @@ const pool = new Pool({
 });
 
 let lastFetchedDate = '1.1.2023';
+
+let message = '';
+const logFile = 'dataOperationsLog';
+
 cron.schedule('*/5 15 * * *', () => {
     try {
         let timestamp = new Date();
         let dateStr = timestamp.toLocaleDateString();
 
         if (lastFetchedDate != dateStr) {
-            console.log('Started fetching price data ');
+            /*console.log('Started fetching price data ');*/
+
+            message = 'Started fetching price data';
+            console.log(message);
+            logger.add2log(message, logFile);
+
+
             priceService.fetchLatestPriceData().then((json) => {
                 json.prices.forEach(async (element) => {
                     let values = [element.startDate, element.price];
@@ -26,15 +37,32 @@ cron.schedule('*/5 15 * * *', () => {
                         let resultset = await pool.query(sqlClause, values);
                         return resultset;
                     }
-                    runQuery().then((resultset) => console.log(resultset.rows[0]))
-                });
+                    runQuery().then((resultset) => {
+                        if (resultset.rows[0] != undefined) {
+                            message = 'Added a row'
+                        }
+                        else {
+                            message = 'Sipped an existing row'
+                        }
+                        console.log(message);
+                        logger.add2log(message, logFile)
+                    })
             });
+        });
             lastFetchedDate = dateStr;
-            console.log('Fetched at', lastFetchedDate);
+
+            message = 'Fetched at ' + lastFetchedDate;
+            console.log(message);
+            logger.add2log(message, logFile);
+           
         } else {
-            console.log('Data has been successfully retrieved earlier today');
+            message = 'Next scheduled event: Data has been successfully retrieved earlier today';
+            console.log(message);
+            logger.add2log(message, logFile);
         }
     } catch (error) {
-        console.log('An error() occurred, trying again in 5 minutes until 4 PM');
+        message = 'An error occured (' + error.toString() + '), trying again in 5 minutes until 4 PM';
+        console.log(message);
+        logger.add2log(message, logFile);
     }
 });
