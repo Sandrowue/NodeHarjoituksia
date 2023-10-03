d542d4c2ef66975bce3e2cc3489fb163b8ea278e
SANDRO WUTHRICH <aleksi.w@protonmail.com>
2023-10-03 14:42:20 +0300

    \n

--- logger.js
+++ logger.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-const timestamp = new Date();
+/*const timestamp = new Date();
 const isoTimeStamp = timestamp.toISOString();
 let operation = 'Data Fetch operation';
 let status = 'processes successfully';
-let entry = operation + ' ' + status + ' @ ' + isoTimeStamp + '/n'
+let entry = operation + ' ' + status + ' @ ' + isoTimeStamp + '\n'
 
 console.log(entry);
 
@@ -12,8 +12,23 @@ fs.appendFile('dataOperation.log', entry, (err) => {
     if (err) {
         console.log(err);
     }
-})
+})*/
 
+const add2log = (entry, fileName) => {
+    const isoTimeStamp = new Date().toISOString();
+    const logRow = entry + '@' + isoTimeStamp + '\n'
+    fs.appendFile(fileName, logRow, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    })
+}
+
+add2log('This is an informational message', 'dataOperation.log')
+
+module.exports = {
+    add2log
+}
 
 
 
